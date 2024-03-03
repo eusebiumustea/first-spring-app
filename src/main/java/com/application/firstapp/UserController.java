2d28013c193bbc865ff74ecc6e4c16fb41664411
eusebiumustea <musteaeusebiu14@gmail.com>
2024-03-03 13:48:18 +0200

    greetings

--- src/main/java/com/application/firstapp/UserController.java
+++ src/main/java/com/application/firstapp/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @GetMapping(path = "/hello")
+    @GetMapping(path = "/greetz")
    private String Hello() {
         return  "Hello";
     }
