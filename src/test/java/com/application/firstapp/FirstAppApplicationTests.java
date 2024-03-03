f067697a3b1ce46250ada4af71c1e7a153cb3b3c
eusebiumustea <musteaeusebiu14@gmail.com>
2024-03-03 13:34:17 +0200

    Initial commit

---
+++ src/test/java/com/application/firstapp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
